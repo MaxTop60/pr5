dd04fb4098cdf46e8d944abc0e9e8cc9631ed3c7
MaxTop60 <150716393+MaxTop60@users.noreply.github.com>
2024-05-20 07:55:01 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <list>
+
+template <typename T>
+class Stack {
+private:
+    std::list<T> data; // Используем List для хранения данных стека
+
+public:
+    // Конструктор по умолчанию
+    Stack() {}
+
+    // Добавление элемента в вершину стека
+    void push(const T& item) {
+        data.push_back(item);
+    }
+
+    // Удаление элемента из вершины стека
+    void pop() {
+        if (!isEmpty()) {
+            data.pop_back();
+        }
+        else {
+            std::cerr << "Stack is empty. Cannot pop.\n";
+        }
+    }
+
+    // Получение элемента из вершины стека без его удаления
+    T& top() {
+        if (!isEmpty()) {
+            return data.back();
+        }
+        else {
+            throw std::out_of_range("Stack is empty. Cannot get top element.");
+        }
+    }
+
+    // Проверка на пустоту стека
+    bool isEmpty() const {
+        return data.empty();
+    }
+
+    // Получение размера стека
+    size_t size() const {
+        return data.size();
+    }
+};
+
+int main() {
+    Stack<int> stack;
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    std::cout << "Top element: " << stack.top() << std::endl;
+    stack.pop();
+    std::cout << "Top element after pop: " << stack.top() << std::endl;
+
+    return 0;
+}
---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+
+template <class T>
+class ListNode {
+public:
+    T data;
+    ListNode<T>* next;
+
+    // Конструктор для создания узла списка с заданным значением
+    ListNode(const T& value) : data(value), next(nullptr) {}
+};
+
+template <class T>
+class LinkedList {
+private:
+    ListNode<T>* head; // Указатель на начало списка
+
+public:
+    // Конструктор по умолчанию
+    LinkedList() : head(nullptr) {}
+
+    // Деструктор для освобождения памяти, занятой узлами списка
+    ~LinkedList() {
+        while (head != nullptr) {
+            ListNode<T>* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    // Метод для добавления элемента в начало списка
+    void insert(const T& value) {
+        ListNode<T>* newNode = new ListNode<T>(value);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    // Метод для удаления первого элемента списка
+    void remove() {
+        if (head != nullptr) {
+            ListNode<T>* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    // Метод для вывода содержимого списка
+    void display() {
+        ListNode<T>* current = head;
+        while (current != nullptr) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    LinkedList<int> myList;
+
+    myList.insert(5);
+    myList.insert(10);
+    myList.insert(15);
+
+    std::cout << "List: ";
+    myList.display();
+
+    myList.remove();
+
+    std::cout << "List after removal: ";
+    myList.display();
+
+    return 0;
+}
